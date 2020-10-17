af458be5965f34528b0db4f75447b24b0673931e
Shanko27 <Shanko.i27@gmail.com>
2020-10-17 17:50:48 +0300

    number1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AllUsersComponent } from './components/all-users/all-users.component';
+import {Route, RouterModule, Routes} from "@angular/router";
+import { UserComponent } from './components/user/user.component';
+
+const routes: Routes = [
+  {path: 'all-users', component: AllUsersComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllUsersComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
