637a8d56c92b1369d524011495753ef6d98203e9
Shanko27 <Shanko.i27@gmail.com>
2020-11-02 13:06:55 +0200

    add user view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AllUsersComponent } from './components/all-users/all-users.component';
-import {Route, RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import {HttpClientModule} from '@angular/common/http';
 import { DetailsUserComponent } from './components/details-user/details-user.component';
+import { AllPostsComponent } from './components/all-posts/all-posts.component';
+import { DetailsPostComponent } from './components/details-post/details-post.component';
+import { PostComponent } from './components/post/post.component';
+import { UserPostComponent } from './components/user-post/user-post.component';
 
 const routes: Routes = [
-  {path: 'all-users', component: AllUsersComponent, children:[
-    {path: 'details/:id', component: DetailsUserComponent}
-    ]}];
+  {path: 'all-users', component: AllUsersComponent, children: [
+    {path: 'details/:id', component: DetailsUserComponent, children: [
+        {path: 'postdetals/:id', component: DetailsPostComponent}
+      ]}
+    ]},
+  {path: 'all-posts', component: AllPostsComponent, children: [
+      {path: 'details/:id', component: DetailsPostComponent}
+    ]}
+    ];
 
 @NgModule({
   declarations: [
     AppComponent,
     AllUsersComponent,
     UserComponent,
-    DetailsUserComponent
+    DetailsUserComponent,
+    AllPostsComponent,
+    DetailsPostComponent,
+    PostComponent,
+    UserPostComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/details-user/details-user.component.ts
+++ src/app/components/details-user/details-user.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {UserService} from '../../service/user.service';
+import {DetalPost} from '../../models/detalPost';
+import {PostService} from '../../service/post.service';
 
 @Component({
   selector: 'app-details-user',
@@ -8,11 +10,15 @@ import {UserService} from '../../service/user.service';
   styleUrls: ['./details-user.component.css']
 })
 export class DetailsUserComponent implements OnInit {
+  @Input()
+  detailedPostt: DetalPost;
 detailedUser;
+
   constructor(private activatedRoute: ActivatedRoute,
-              private userService: UserService) {
+              private userService: UserService, private postService: PostService) {
     this.activatedRoute.params.subscribe((params) => {
       this.userService.getUserId(params.id).subscribe(user => this.detailedUser = user);
+      this.postService.getPostId(params.id).subscribe(post => this.detailedPostt = post);
     });
   }
 
